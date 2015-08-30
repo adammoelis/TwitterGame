f8a2eb00e40416e85611ef3d0e80810d28b7d72b
Adam Moelis <adam.moelis@gmail.com>
2015-08-30 18:36:06 -0400

    images sources added to index view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828201546) do
+ActiveRecord::Schema.define(version: 20150830222023) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "text"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150828201546) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
   end
 
 end
